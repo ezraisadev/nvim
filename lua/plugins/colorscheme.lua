80fddcdf8801d40b4d1763b18ba8af1d24dc6325
Ezra <128799204+ezraisadev@users.noreply.github.com>
2023-05-26 19:32:19 +0200

    chore(colorscheme): change colorscheme to onedark pro

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,31 +1,12 @@
 local M = {
-  "rebelot/kanagawa.nvim",
+  "olimorris/onedarkpro.nvim",
   lazy = false,
   priority = 1000
 }
 
-M.name = "kanagawa"
+M.name = "onedark"
 
 function M.config()
-  require("kanagawa").setup({
-    compile = true,
-    colors = {
-      theme = {
-        wave = {
-          ui = {
-            bg_gutter = "none",
-          },
-        },
-      },
-    },
-    overrides = function(colors)
-      return {
-        LineNr = {fg = colors.palette.crystalBlue, bg = "none"},
-        LineNrAbove = {fg = colors.palette.sumiInk6, bg = "none"},
-        LineNrBelow = {fg = colors.palette.sumiInk6, bg = "none"},
-      }
-    end,
-  })
   local status_ok, _ = pcall(vim.cmd.colorscheme, M.name)
   if not status_ok then
     vim.api.nvim_err_writeln("Failed to load " .. M.name .. "\n\n" .. fault)
