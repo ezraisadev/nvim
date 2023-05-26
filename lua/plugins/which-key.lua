21785b5edabd3758681320e66308621c1436ad3a
Ezra <128799204+ezraisadev@users.noreply.github.com>
2023-05-26 18:39:10 +0200

    chore(which-key): update group names

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -4,8 +4,32 @@ local M = {
 }
 
 function M.config()
+  local get_icon = require("core.utils").get_icon
   local wk = require "which-key"
-  wk.setup{}
+  wk.setup{
+    icons = {
+      group = "",
+    },
+  }
+
+  local names = {
+    f =  get_icon("Search", 1, true) .. "Find",
+    p =  get_icon("Package", 1, true) .. "Packages",
+    l =  get_icon("ActiveLSP", 1, true) .. "LSP",
+    u =  get_icon("Window", 1, true) .. "UI",
+    b =  get_icon("Tab", 1, true) .. "Buffers",
+    bs = get_icon("Sort", 1, true) .. "Sort Buffers",
+    d =  get_icon("Debugger", 1, true) .. "Debugger",
+    g =  get_icon("Git", 1, true) .. "Git",
+    S =  get_icon("Session", 1, true) .. "Session",
+    t =  get_icon("Terminal", 1, true) .. "Terminal",
+  }
+  
+  wk.register({
+    f = { name = names.f },
+    q = { name = get_icon("Quit", 1, true) .. "Quit" },
+    w = { name = get_icon("Save", 1, true) .. "Save" },
+  }, { prefix = "<leader>" })
 end
 
 return M
