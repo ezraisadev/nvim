61e691cb92596ec40d1e43f4cd1f0234743b0b77
Ezra <128799204+ezraisadev@users.noreply.github.com>
2023-05-27 23:32:21 +0200

    feat(cmp): add cmp

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,89 @@
+local M = {
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    {
+      "L3MON4D3/LuaSnip",
+      event = "InsertEnter",
+      dependencies = {
+        "rafamadriz/friendly-snippets",
+      },
+    },
+    "saadparwaiz1/cmp_luasnip",
+  }
+}
+
+function M.config()
+  local cmp = require("cmp")
+  local luasnip = require("luasnip")
+  require('luasnip.loaders.from_vscode').lazy_load()
+
+  cmp.setup{
+    snippet = {
+      expand = function(args)
+        luasnip.lsp_expand(args.body)
+      end
+    },
+    sources = {
+      {name = 'nvim_lsp', keyword_length = 1},
+      {name = 'luasnip', keyword_length = 2},
+      {name = 'buffer', keyword_length = 3},
+      {name = 'path'},
+    },
+    mapping = {
+      ['<Up>'] = cmp.mapping.select_prev_item(select_opts),
+      ['<Down>'] = cmp.mapping.select_next_item(select_opts),
+
+      ['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
+      ['<C-n>'] = cmp.mapping.select_next_item(select_opts),
+
+      ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-d>'] = cmp.mapping.scroll_docs(4),
+
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<C-y>'] = cmp.mapping.confirm({select = true}),
+      ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+      ['<C-f>'] = cmp.mapping(function(fallback)
+        if luasnip.jumpable(1) then
+          luasnip.jump(1)
+        else
+          fallback()
+        end
+      end, {'i', 's'}),
+
+      ['<C-b>'] = cmp.mapping(function(fallback)
+        if luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, {'i', 's'}),
+
+      ['<Tab>'] = cmp.mapping(function(fallback)
+        local col = vim.fn.col('.') - 1
+
+        if cmp.visible() then
+          cmp.select_next_item(select_opts)
+        elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+          fallback()
+        else
+          cmp.complete()
+        end
+      end, {'i', 's'}),
+
+      ['<S-Tab>'] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item(select_opts)
+        else
+          fallback()
+        end
+      end, {'i', 's'}),
+    },
+  }
+end
+
+return M
