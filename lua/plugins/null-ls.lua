455f0903bd112fc15166adbf0b61f96e41f73a43
Ezra <128799204+ezraisadev@users.noreply.github.com>
2023-05-28 18:09:25 +0200

    feat(null-ls): add null-ls

---
+++ lua/plugins/null-ls.lua
@@ -0,0 +1,64 @@
+local M = {
+  "jose-elias-alvarez/null-ls.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    { "mason.nvim" },
+    {
+      "nvim-lua/plenary.nvim",
+      lazy = true
+    },
+  },
+}
+
+function M.config()
+  local null_ls = require "null-ls"
+  local formatting = null_ls.builtins.formatting
+  local diagnostics = null_ls.builtins.diagnostics
+  local lsp_formatting = function(bufnr)
+    vim.lsp.buf.format({
+      filter = function(client)
+        -- Ignore formatting from these LSPs
+        local lsp_formatting_denylist = {
+          eslint = true,
+          lemminx = true,
+          lua_ls = true,
+        }
+        if lsp_formatting_denylist[client.name] then
+          return false
+        end
+        return true
+      end,
+      bufnr = bufnr,
+    })
+  end
+  local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+  null_ls.setup({
+    on_attach = function(client, bufnr)
+      if client.supports_method("textDocument/formatting") then
+        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          group = augroup,
+          buffer = bufnr,
+          callback = function()
+            lsp_formatting(bufnr)
+          end,
+        })
+      end
+    end,
+    debug = false,
+    sources = {
+      formatting.prettierd.with {
+        extra_filetypes = { "toml" },
+        --extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+      },
+      formatting.black.with { extra_args = { "--fast" } }, -- python
+      formatting.stylua,
+      formatting.eslint_d,
+      diagnostics.flake8,  -- python
+      diagnostics.pylint,
+    },
+  })
+end
+
+return M
