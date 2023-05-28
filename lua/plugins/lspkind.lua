5be1dd30cfc4eab3cb7b14014a0a95b2f2d724b7
Ezra <128799204+ezraisadev@users.noreply.github.com>
2023-05-28 14:21:48 +0200

    chore(lspkind): update lspkind

--- lua/plugins/lspkind.lua
+++ lua/plugins/lspkind.lua
@@ -1,31 +1,33 @@
 local M = {
 
-    "onsails/lspkind.nvim",
-    opts = {
-      mode = "symbol",
-      symbol_map = {
-        Array = "󰅪",
-        Boolean = "⊨",
-        Class = "󰌗",
-        Constructor = "",
-        Key = "󰌆",
-        Namespace = "󰅪",
-        Null = "NULL",
-        Number = "#",
-        Object = "󰀚",
-        Package = "󰏗",
-        Property = "",
-        Reference = "",
-        Snippet = "",
-        String = "󰀬",
-        TypeParameter = "󰊄",
-        Unit = "",
-      },
+  "onsails/lspkind.nvim",
+  opts = {
+    preset = "default",
+    mode = "symbol",
+    symbol_map = {
+      Array = "󰅪",
+      Boolean = "⊨",
+      Class = "󰌗",
+      Constructor = "",
+      Key = "󰌆",
+      Namespace = "󰅪",
+      Null = "NULL",
+      Number = "#",
+      Object = "󰀚",
+      Package = "󰏗",
+      Property = "",
+      Reference = "",
+      Snippet = "",
+      String = "󰀬",
+      TypeParameter = "󰊄",
+      Unit = "",
     },
-    enabled = vim.g.icons_enabled,
+  },
+  --enabled = vim.g.icons_enabled,
+  event = "VeryLazy"
 }
 
-function M.config()
+function M.config(_, opts)
   require("lspkind").init(opts)
 end
 
