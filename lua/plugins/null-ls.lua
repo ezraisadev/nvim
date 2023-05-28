17ffb6dd8adead6789ba934c841d544892c56061
Ezra <128799204+ezraisadev@users.noreply.github.com>
2023-05-28 19:31:46 +0200

    chore(null-ls): update eslint_d

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -12,6 +12,7 @@ local M = {
 
 function M.config()
 	local null_ls = require("null-ls")
+	local code_actions = null_ls.builtins.code_actions
 	local formatting = null_ls.builtins.formatting
 	local diagnostics = null_ls.builtins.diagnostics
 	local lsp_formatting = function(bufnr)
@@ -54,7 +55,8 @@ function M.config()
       }, ]]
 			formatting.black.with({ extra_args = { "--fast" } }), -- python
 			formatting.stylua,
-			formatting.eslint_d,
+			code_actions.eslint_d,
+			diagnostics.eslint_d,
 			diagnostics.flake8, -- python
 			diagnostics.pylint,
 		},
