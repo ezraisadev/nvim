c6f7a80d75be1ff97b757fa8551aebfba76feeb3
Ezra <128799204+ezraisadev@users.noreply.github.com>
2023-05-30 12:32:20 +0200

    chore(treesitter): overwrite

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,35 +1,35 @@
 local M = {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPost", "BufNewFile" },
-  build = function()
-    require("nvim-treesitter.install").update({ with_sync = true })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPost", "BufNewFile" },
+	build = function()
+		require("nvim-treesitter.install").update({ with_sync = true })
+	end,
 }
 
 function M.config()
-  require("nvim-treesitter.configs").setup({
-    ensure_installed = {
-      "c",
-      "css",
-      "html",
-      "javascript",
-      "json",
-      "lua",
-      "luadoc",
-      "luap",
-      "python",
-      "query",
-      "regex",
-      "tsx",
-      "typescript",
-      "vim",
-      "vimdoc",
-      "yaml"
-    },
-    highlight = { enable = true },
-    indent = { enable = false, disable = { "python", "css" } },
-    incremental_selection = { enable = true },
-  })
+	require("nvim-treesitter.configs").setup({
+		ensure_installed = {
+			"c",
+			"css",
+			"html",
+			"javascript",
+			"json",
+			"lua",
+			"luadoc",
+			"luap",
+			"python",
+			"query",
+			"regex",
+			"tsx",
+			"typescript",
+			"vim",
+			"vimdoc",
+			"yaml",
+		},
+		highlight = { enable = true },
+		indent = { enable = false, disable = { "python", "css" } },
+		incremental_selection = { enable = true },
+	})
 end
 
 return M
